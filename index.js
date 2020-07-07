b253b3d873676af3589a3a2c90c79a256ba86579
JIWON LEE <dlwldnjs9690@gmail.com>
2020-07-07 12:19:53 +0900

    secrete

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
 
+const config = require('./config/key');
+
+const {User} = require("./models/User");
+
+//application/x-ww-form-urlencoded형태로 분석해서 가져옴
+app.use(bodyParser.urlencoded({extended: true}));
+//applicatino/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jiwon:1234@boilerplate.cnpyh.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected...'))
 .catch(err=>console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! 안녕 지워나'))
+
+
+app.post('/register', (req, res)=>{
+    
+    //회원가입시 필요한 정보들을 client에서 가져오면 DB에 넣어준다.
+
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- models/User.js
+++ models/User.js
@@ -22,10 +22,17 @@ const userSchema = mongoose.Schema({
         type: Number,
         default: 0
     },
-    image:{
-        image: String,
-        token: {
-            type: String
-        }
+    image:String,
+    token: {            //유효성관리
+        type: String
+    },
+    tokenExp: {         //토큰 유효기간 등
+        type: Number
     }
 })
+
+//스키마를 모델로 감싸기
+const User = mongoose.model('User', userSchema)
+
+//다른 곳에서 쓸 수 있도록 설정
+module.exports={User}
